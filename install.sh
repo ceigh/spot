7235fbf8e2e32795382c714d9404f9dff002bc11
Artem Lebzak <hilth@protonmail.com>
2018-01-18 22:52:32 +0500

    Release 0.5

--- install.sh
+++ install.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
-sudo mkdir /opt/spot
-here=`pwd`
-sudo cp $here/coinHiveMiner.js /opt/spot/
-sudo cp $here/spot /usr/local/bin/
-sudo chmod +x /usr/local/bin/spot
-sudo apt update
-apt-get install python-dev python-setuptools libpcap0.8-dev libnetfilter-queue-dev libssl-dev libjpeg-dev libxml2-dev libxslt1-dev libcapstone3 libcapstone-dev libxml2-dev libxslt1-dev python-dnslib python-magic python-netfilterqueue python-ua-parser python-user-agents
-pip install requests[security]
-cd /opt
-sudo git clone https://github.com/byt3bl33d3r/MITMf
-cd MITMf && git submodule init && git submodule update --recursive
-pip install -r requirements.txt
-echo
-echo -e "\033[1m\e[35mNow you can remove this directory and use spot!\033[0m"
-echo
+confirm=''
+echo -e "\033[1m\e[35mInstallation may take a long time, please confirm with Enter\nor ^C to exit\033[0m"
+read confirm
+if [[ $confirm -eq '' ]]
+then
+  sudo mkdir /opt/spot > /dev/null 2>&1
+  here=`pwd`
+  sudo cp -n $here/coinHiveMiner.js $here/logo.txt $here/gateway.txt $here/interface.txt /opt/spot/
+  sudo cp -f $here/spot /usr/local/bin/spot
+  sudo chmod +x /usr/local/bin/spot
+  
+  echo -e "\033[1m\e[35mInstallation...\033[0m"
+  sudo apt-get update > /dev/null 2>&1
+  echo -ne '\n' | sudo apt-get install python-pip > /dev/null 2>&1
+  pip install --upgrade pip > /dev/null 2>&1
+  echo -ne '\n' | sudo apt-get install git python-dev python-setuptools libpcap0.8-dev libnetfilter-queue-dev libssl-dev libjpeg-dev libxml2-dev libxslt1-dev libcapstone3 libcapstone-dev libxml2-dev libxslt1-dev python-magic > /dev/null 2>&1
+  sudo pip install dnslib NetfilterQueue ua-parser user-agents > /dev/null 2>&1
+  sudo pip install requests[security] > /dev/null 2>&1
+
+  cd /opt
+  sudo git clone https://github.com/byt3bl33d3r/MITMf > /dev/null 2>&1
+  cd /opt/MITMf && sudo git submodule init && sudo git submodule update --recursive > /dev/null 2>&1
+
+  sudo pip install -r requirements.txt > /dev/null 2>&1
+
+  echo -ne '\n' | sudo apt-get autoremove > /dev/null 2>&1
+  echo -e "\033[1m\e[35mNow you can remove this directory and use spot!\033[0m"
+else
+  echo -e "\033[1m\e[35mStop\033[0m"
+  exit 0
+fi
